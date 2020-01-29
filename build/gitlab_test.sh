d9ce08cd500dfe3dc484c0871861e97ce9053a56
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2020-01-29 13:46:53 +0100

    reverted test

--- build/gitlab_test.sh
+++ build/gitlab_test.sh
@@ -5,7 +5,4 @@ set -e
 echo $CI_REGISTRY_PASSWORD | docker login -u $CI_REGISTRY_USER --password-stdin  $CI_REGISTRY
 
 docker-compose -f build/docker-compose.yml pull
-# docker-compose -f build/docker-compose.yml run icat /build/build/gitlab_test_in_container.sh
-docker-compose -f build/docker-compose.yml up
-
-docker-compose -f build/docker-compose.yml down
+docker-compose -f build/docker-compose.yml run icat /build/build/gitlab_test_in_container.sh
