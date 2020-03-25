bd344781e29c93553012763c543128ae4104e18d
Stefan Wolfsheimer <stefan.wolfsheimer@gmail.com>
2020-03-26 00:16:05 +0100

    test echo registry

--- build/gitlab_test.sh
+++ build/gitlab_test.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 set -x
 set -e
-
+echo $REGISTRY
 docker-compose -f build/docker-compose.yml pull
 docker-compose -f build/docker-compose.yml run icat /build/build/gitlab_test_in_container.sh
